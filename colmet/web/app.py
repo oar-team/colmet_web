9114799380112c23cf8d651e169d3a1f80f30d31
Salem Harrache <salem.harrache@inria.fr>
2014-01-19 19:54:27 +0100

    removed custom errorhandlers

--- colmet/web/app.py
+++ colmet/web/app.py
@@ -4,7 +4,6 @@ from __future__ import division, unicode_literals
 from flask import Flask
 
 from .context import configure_context_processors
-from .exceptions import configure_errorhandlers
 from .extensions import configure_extensions
 from .filters import configure_template_filters
 from .logger import configure_logging
@@ -12,14 +11,15 @@ from .views import register_blueprints
 from .config import DefaultConfig
 
 
-def create_app(config_file=None):
-    app = Flask("colmet")
+app = Flask(__name__)
+
+
+def configure_app(app, config_file=None):
     app.jinja_env.trim_blocks = True
     app.config.from_object(DefaultConfig)
     app.config.from_envvar('COLMET_WEB_CONFIG', silent=True)
     if config_file is not None:
         app.config.from_pyfile(config_file, silent=True)
-    configure_errorhandlers(app)
     configure_extensions(app)
     configure_template_filters(app)
     configure_context_processors(app)
