915dec3784d14a980c502ced54a48bf3f9e46747
Salem Harrache <salem.harrache@inria.fr>
2014-01-20 14:41:38 +0100

    removed flask-assets

--- colmet/web/extensions.py
+++ colmet/web/extensions.py
@@ -3,16 +3,13 @@ from __future__ import division, unicode_literals
 
 from flask_cache import Cache
 from flask_debugtoolbar import DebugToolbarExtension
-from flask.ext.assets import Environment
 
 
 cache = Cache()
-assets = Environment()
 
 
 def configure_extensions(app):
     cache.init_app(app)
-    assets.init_app(app)
 
     if app.debug:
         DebugToolbarExtension(app)
